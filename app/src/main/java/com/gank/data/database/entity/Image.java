89c8366380875e9a5598133e1856c3abc2b1457b
yanyiqun <287979495@qq.com>
2017-06-06 10:29:43 +0800

    gif

--- app/src/main/java/com/gank/data/database/entity/Image.java
+++ app/src/main/java/com/gank/data/database/entity/Image.java
@@ -1,5 +1,8 @@
 package com.gank.data.database.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Generated;
@@ -8,7 +11,7 @@ import org.greenrobot.greendao.annotation.Generated;
  * Created by Administrator on 2017/4/17 0017.
  */
 @Entity
-public class Image {
+public class Image implements Parcelable {
     @Id
     Long id;
     String imageUrl;
@@ -44,5 +47,34 @@ public class Image {
     public void setImageId(String imageId) {
         this.imageId = imageId;
     }
-  
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.imageUrl);
+        dest.writeString(this.imageId);
+    }
+
+    protected Image(Parcel in) {
+        this.id = (Long) in.readValue(Long.class.getClassLoader());
+        this.imageUrl = in.readString();
+        this.imageId = in.readString();
+    }
+
+    public static final Parcelable.Creator<Image> CREATOR = new Parcelable.Creator<Image>() {
+        @Override
+        public Image createFromParcel(Parcel source) {
+            return new Image(source);
+        }
+
+        @Override
+        public Image[] newArray(int size) {
+            return new Image[size];
+        }
+    };
 }
